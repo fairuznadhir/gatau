dd0f3c7f791f12bb690e227d4aa53ce0fa95abd5
fairuznadhir <161941434+fairuznadhir@users.noreply.github.com>
2024-03-17 22:36:27 +0700

    Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,21 +1,7 @@
 #include "Player.h"
 
-Player::Player() : score(0) {}
-
-Player::Player(const std::string& playerName) : name(playerName), score(0) {}
-
-void Player::setName(const std::string& playerName) {
-    name = playerName;
-}
+Player::Player(const std::string& playerName) : name(playerName) {}
 
 std::string Player::getName() const {
     return name;
-}
-
-void Player::increaseScore(int points) {
-    score += points;
-}
-
-int Player::getScore() const {
-    return score;
 }
