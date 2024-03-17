a83118fc39ca7c7b5cc0f5fcf197d0a92ca7b9d3
fairuznadhir <161941434+fairuznadhir@users.noreply.github.com>
2024-03-17 22:38:52 +0700

    GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,35 +1,24 @@
 #include "GameManager.h"
 #include <iostream>
 
-GameManager::GameManager(int boardSize) : board(boardSize), currentPlayerIndex(0) {}
+GameManager::GameManager(int boardSize, const std::string& playerName) : board(boardSize), player(playerName) {}
 
 void GameManager::startGame() {
-    std::cout << "Welcome to Sudoku!" << std::endl;
-    Player player1("Player 1");
-    Player player2("Player 2");
-    players.push_back(player1);
-    players.push_back(player2);
+    std::cout << "Welcome to Sudoku, " << player.getName() << "!" << std::endl;
 
-    while (!checkWin() && !board.isFull()) {
-        Player& currentPlayer = players[currentPlayerIndex];
+    while (!board.isFull() && !board.isWin()) {
         board.printBoard();
-        std::cout << currentPlayer.getName() << "'s turn:" << std::endl;
-        getPlayerInput(currentPlayer);
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+        getPlayerInput();
     }
 
-    if (checkWin()) {
-        std::cout << "Congratulations! " << players[currentPlayerIndex].getName() << " wins!" << std::endl;
+    if (board.isWin()) {
+        std::cout << "Congratulations! You completed the puzzle!" << std::endl;
     } else {
-        std::cout << "It's a draw!" << std::endl;
+        std::cout << "Game over! You didn't complete the puzzle." << std::endl;
     }
 }
 
-bool GameManager::checkWin() {
-    return board.isWin();
-}
-
-void GameManager::getPlayerInput(Player& player) {
+void GameManager::getPlayerInput() {
     int row, col, value;
     do {
         std::cout << "Enter row, column, and value (1-" << board.getSize() << ") separated by space: ";
